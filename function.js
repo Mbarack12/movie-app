3e77919d130a223bb716897f5529c41f9a3e9d55
mbarack <duncanayida@icloud .com>
2025-08-16 06:52:41 -0700

    update

--- function.js
+++
@@ -1,4 +0,0 @@
-let myfunction = function( firstnumber,secondnumber){
-    alert(firstnumber+secondnumber)
-}
-myfunction(5,5)
---
+++ js/function.js
@@ -0,0 +1,4 @@
+let myfunction = function( firstnumber,secondnumber,thirdnumber){
+    alert(firstnumber-secondnumber+thirdnumber)
+}
+myfunction(5,5,4)
